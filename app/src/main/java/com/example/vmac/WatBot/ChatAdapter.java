6cc1c8d21c1a3b1d53c0fb3525e5ab59ee40a037
Marco A. Peyrot <mpeyrotc@Marcos-MacBook-Pro.local>
2017-09-28 15:48:50 -0500

    Add docs for ChatAdapter and separate images into 3

--- app/src/main/java/com/example/vmac/WatBot/ChatAdapter.java
+++ app/src/main/java/com/example/vmac/WatBot/ChatAdapter.java
@@ -1,32 +1,35 @@
 package com.example.vmac.WatBot;
 
-/**
- * Created by VMac on 17/11/16.
- */
-
-import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * Created by VMac on 17/11/16.
+ * Modified by mpeyrotc on 28/09/17.
+ *
+ * The ChatAdapter extends a {@code RecycleView} and acts as a holder for the
+ * message boxes generated between Watson and the user. It has a message size limit
+ * of 100 and holds the mappings for showing helpful images to the user if requested.
+ *
+ * @author VMac
+ * @author mpeyrotc
+ * @version 1.0
+ */
 public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private int SELF = 100;
-    private ArrayList<Message> messageArrayList;
-    private Map<String, Integer> images;
+    private final int SELF = 100;
+    private final ArrayList<Message> messageArrayList;
+    private final Map<String, Integer> images;
 
     {
         images = new HashMap<>();
-        images.put("Ponga sus brazos alrededor de la cintura de la persona que se está asfixiando. Inclínelo un poco hacia delante de su cintura. Haga un puño con una de sus manos. Coloque el lado del dedo pulgar de su puño entre el ombligo de la persona y la parte más baja de sus costillas. No coloque su puño sobre las costillas. Coloque la otra mano sobre su puño. Presione su puño contra el abdomen de la persona con una compresión rápida hacia adentro y hacia arriba. Repita las compresiones hasta que el objeto salga. Si la persona vomita, acuéstelo de lado para evitar que el objeto obstruya totalmente sus vías respiratorias.",
+        images.put("Ponga sus brazos alrededor de la cintura de la persona vertical que se está asfixiando. Inclínelo un poco hacia delante de su cintura. Haga un puño con una de sus manos. Coloque el lado del dedo pulgar de su puño entre el ombligo de la persona y la parte más baja de sus costillas. No coloque su puño sobre las costillas. Coloque la otra mano sobre su puño. Presione su puño contra el abdomen de la persona con una compresión rápida hacia adentro y hacia arriba. Repita las compresiones hasta que el objeto salga. Si la persona vomita, acuéstelo de lado para evitar que el objeto obstruya totalmente sus vías respiratorias.",
                 R.drawable.imagen_1);
         images.put("Se está atragantando con algo? Si no, realiza respiración boca a boca 12 veces cada minuto hasta que pueda respirar por su cuenta. Sigue alerta de ella.",
                 R.drawable.imagen_2);
@@ -34,11 +37,22 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 R.drawable.imagen_2);
         images.put("Realiza ciclos de 30 segundos donde hagas 30 compresiones torácicas y 2 insuflaciones",
                 R.drawable.imagen_3);
+        images.put("El auxiliante se coloca arrodillado y con sus manos una sobre la otra debe presionar fuerte mente la parte alta del abdomen (por debajo de las costillas) para hacer subir violentamente el diafragma y provocar la salida brusca del aire de los pulmones; de esta forma se expulsa el objeto de las vías respiratorias.",
+                R.drawable.imagen_4);
+        images.put("Ponga sus brazos alrededor de la cintura de la persona sentada que se está asfixiando. Inclínelo un poco hacia delante de su cintura. Haga un puño con una de sus manos. Coloque el lado del dedo pulgar de su puño entre el ombligo de la persona y la parte más baja de sus costillas. No coloque su puño sobre las costillas. Coloque la otra mano sobre su puño. Presione su puño contra el abdomen de la persona con una compresión rápida hacia adentro y hacia arriba. Repita las compresiones hasta que el objeto salga. Si la persona vomita, acuéstelo de lado para evitar que el objeto obstruya totalmente sus vías respiratorias.",
+                R.drawable.imagen_5);
     }
 
 
+    /**
+     * Default constructor for the adapter. When created, receives a reference to the messages
+     * held by the {@link com.example.vmac.WatBot.MainActivity MainActivity}.
+     *
+     * @param messageArrayList the {@code List} that contains the messages exchanged between
+     *                         Watson and the user.
+     */
     public ChatAdapter(ArrayList<Message> messageArrayList) {
-        this.messageArrayList=messageArrayList;
+        this.messageArrayList = messageArrayList;
 
     }
 
@@ -65,7 +79,7 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemViewType(int position) {
         Message message = messageArrayList.get(position);
-        if (message.getId()!=null && message.getId().equals("1")) {
+        if (message.getId() != null && message.getId().equals("1")) {
             return SELF;
         }
 
@@ -77,33 +91,43 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         Message message = messageArrayList.get(position);
         message.setMessage(message.getMessage());
         ((ViewHolder) holder).message.setText(message.getMessage());
-        }
+    }
 
     @Override
     public int getItemCount() {
-            return messageArrayList.size();
+        return messageArrayList.size();
     }
 
+    /**
+     * The ViewHolder class identifies each message's content (the actual text message) and
+     * determines if the given text should be accompanied by an explanatory image.
+     * If this is the case, the image is appended to the conversation below the corresponding
+     * message sent be Watson.
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
+        // Each message generated during a char conversation
         TextView message;
 
+        /**
+         * The ViewHolder method checks each {@code View} object that represents a message.
+         * If the message matches a given key, the corresponding image that accompanies
+         * such text is appended in an {@code ImageView} object.
+         *
+         * It is worth noting that the match could be made to either a Watson response
+         * or a user's query.
+         *
+         * @param view the {@code View} object that holds a specific message instance.
+         */
         public ViewHolder(View view) {
             super(view);
-            message = (TextView) itemView.findViewById(R.id.message);
-            ImageView image = (ImageView) itemView.findViewById(R.id.watson_image);
+            message = itemView.findViewById(R.id.message);
+            ImageView image = itemView.findViewById(R.id.watson_image);
 
             String key = messageArrayList.get(getItemCount() - 1).getMessage();
 
-            if(images.containsKey(key)) {
+            if (images.containsKey(key)) {
                 image.setImageResource(images.get(key));
             }
-
-            //TODO: Uncomment this if you want to use a custom Font
-            /*String customFont = "Montserrat-Regular.ttf";
-            Typeface typeface = Typeface.createFromAsset(itemView.getContext().getAssets(), customFont);
-            message.setTypeface(typeface);*/
         }
     }
-
-
 }
